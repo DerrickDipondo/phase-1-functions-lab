d0a819208d1c1a8d5aa98e48646e628d0b0d4bcc
DerrickDipondo <derrickdipondo@gmail.com>
2024-11-28 12:10:53 +0300

    function-lab

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+    const hq = 42;
+    return Math.abs(block - hq);
+
+}
+
+function distanceFromHqInFeet(block) {
+    const hq = 42;
+    const feetPerBlock = 88;
+
+    const distanceInBlocks = Math.abs(block - hq);
+
+    return distanceInBlocks * feetPerBlock * 3;
+    
+
+}
+
+function distanceTravelledInFeet(start, destination) {
+const feetPerBlock = 264;
+
+const distanceInBlocks = Math.abs(start - destination);
+
+return distanceInBlocks * feetPerBlock;
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(destination, start);
+
+    
+    if (distance <= 400) {
+        return 0;
+    } 
+    
+    else if (distance > 400 && distance <= 2000) {
+        const chargeableDistance = distance - 400;
+    
+        return 0.02 * (distance - 400);
+    } 
+    
+    else if (distance > 2000 && distance <= 2500) {
+        return 25;
+    } 
+    
+    else {
+        return 'cannot travel that far';
+    }
+
+}
+
